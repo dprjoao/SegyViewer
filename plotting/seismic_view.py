62d0b8fee80c94102341b886f0e3ffb92a92d0b0
João Pedro Reis <jp_reis@id.uff.br>
2026-08-27 16:53:04 -0300

    Reorganize viewer workspace

--- plotting/seismic_view.py
+++ plotting/seismic_view.py
@@ -1,31 +1,130 @@
 import numpy as np
 import pyqtgraph as pg
-from PySide6.QtCore import Signal, Qt
+from PySide6.QtCore import Qt, Signal
+
 
 class SeismicView(pg.PlotWidget):
     traceSelected = Signal(int)
+
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.trace_indices = np.array([], dtype=int)
+
+        self.setBackground("#11171C")
+
+        self.trace_indices = np.array(
+            [],
+            dtype=int,
+        )
+
         self.image = pg.ImageItem()
         self.addItem(self.image)
-        self.setLabel("left","Time",units="ms")
-        self.setLabel("bottom","Trace")
-        self.getViewBox().invertY(True)
-        self.scene().sigMouseClicked.connect(self._clicked)
 
-    def set_section(self, data, indices, dt_ms):
-        self.trace_indices = np.asarray(indices,dtype=int)
-        image = np.asarray(data,dtype=np.float32)
-        self.image.setImage(image, autoLevels=True)
-        if len(indices):
-            x0=float(indices[0])
-            dx=float(indices[1]-indices[0]) if len(indices)>1 else 1.0
-            self.image.setRect(x0,0.0,dx*image.shape[1],dt_ms*image.shape[0])
+        self.setLabel(
+            "left",
+            "Time",
+            units="ms",
+            color="#C8D2DA",
+        )
+        self.setLabel(
+            "bottom",
+            "Trace",
+            color="#C8D2DA",
+        )
+
+        for axis_name in (
+            "left",
+            "bottom",
+        ):
+            axis = self.getAxis(axis_name)
+            axis.setPen("#596874")
+            axis.setTextPen("#B8C4CD")
+
+        self.getViewBox().invertY(True)
+        self.getViewBox().setBorder(
+            pg.mkPen("#394650")
+        )
+
+        self.scene().sigMouseClicked.connect(
+            self._mouse_clicked
+        )
+
+    def set_section(
+        self,
+        data,
+        trace_indices,
+        sample_axis_ms,
+    ):
+        self.trace_indices = np.asarray(
+            trace_indices,
+            dtype=int,
+        )
+
+        image = np.asarray(
+            data,
+            dtype=np.float32,
+        )
+
+        self.image.setImage(
+            image,
+            autoLevels=True,
+        )
+
+        if len(trace_indices):
+            x0 = float(trace_indices[0])
+
+            dx = (
+                float(
+                    trace_indices[1]
+                    - trace_indices[0]
+                )
+                if len(trace_indices) > 1
+                else 1.0
+            )
+
+            if len(sample_axis_ms) > 1:
+                y0 = float(
+                    sample_axis_ms[0]
+                )
+                dy = float(
+                    sample_axis_ms[1]
+                    - sample_axis_ms[0]
+                )
+            else:
+                y0 = 0.0
+                dy = 1.0
+
+            self.image.setRect(
+                x0,
+                y0,
+                dx * image.shape[1],
+                dy * image.shape[0],
+            )
+
         self.autoRange()
 
-    def _clicked(self,event):
-        if event.button()!=Qt.LeftButton or not len(self.trace_indices): return
-        p=self.getViewBox().mapSceneToView(event.scenePos())
-        i=int(self.trace_indices[np.argmin(np.abs(self.trace_indices-p.x()))])
-        self.traceSelected.emit(i)
+    def _mouse_clicked(self, event):
+        if (
+            event.button() != Qt.LeftButton
+            or not len(self.trace_indices)
+        ):
+            return
+
+        point = (
+            self.getViewBox()
+            .mapSceneToView(event.scenePos())
+        )
+
+        trace_index = int(
+            self.trace_indices[
+                np.argmin(
+                    np.abs(
+                        self.trace_indices
+                        - point.x()
+                    )
+                )
+            ]
+        )
+
+        self.traceSelected.emit(
+            trace_index
+        )
